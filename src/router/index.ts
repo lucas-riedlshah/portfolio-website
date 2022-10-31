9a9c323d7f3d98f3576cd40ad14b3b68016b8fac
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2022-10-31 22:39:28 +1300

    added temporary redirect for project pages to go to old website

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,27 +4,42 @@ import Home from "../views/Home.vue";
 import Projects from "../views/Projects.vue";
 import MainMenu from "../views/MainMenu.vue";
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/projects",
-    name: "Projects",
-    component: Projects,
-  },
-  {
-    path: "/menu",
-    name: "MainMenu",
-    component: MainMenu,
-  },
-];
-
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home,
+    },
+    {
+      path: "/projects",
+      name: "Projects",
+      component: Projects,
+    },
+    {
+      path: "/projects/:project",
+      component: Projects,
+    },
+    {
+      path: "/menu",
+      name: "MainMenu",
+      component: MainMenu,
+    },
+  ],
+});
+
+// Temp redirect to old site for projects.
+router.afterEach((guard) => {
+  if (
+    [
+      "/projects/ethereal-deviations",
+      "/projects/ethereal",
+      "/projects/central-vibrance",
+      "/projects/density",
+    ].includes(guard.path)
+  )
+    window.open('https://old.generatively.xyz' + guard.path);
 });
 
 export default router;
