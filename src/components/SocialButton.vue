0689c3dc718c7aef02105861b5b9e237c531b3af
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2022-10-31 20:12:39 +1300

    fix support for dark color scheme

--- src/components/SocialButton.vue
+++ src/components/SocialButton.vue
@@ -12,6 +12,7 @@ defineProps<{ src?: string, href?: string }>()
 
 <style scoped>
 .social-button {
+  --shadow-color: 0, 0, 0;
   --size: clamp(60px, 10vw, 100px);
   width: var(--size);
   height: var(--size);
@@ -21,24 +22,36 @@ defineProps<{ src?: string, href?: string }>()
   font-size: var(--size);
   line-height: 1;
   border-radius: 100%;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 0 10px rgba(var(--shadow-color), 0.1);
+  background-color: white;
   background-size: 70%;
   background-repeat: no-repeat;
   background-position: center center;
-  transition: box-shadow 0.2s;
+  margin: 0.1em 0;
+  transition: box-shadow 0.2s, margin 0.2s;
 }
 
 .social-button:hover {
-  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.15);
+  box-shadow: 0 5px 15px rgba(var(--shadow-color), 0.15);
+  margin: 0.05em 0 0.15em 0;
 }
 
 .social-button:active {
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 10px 20px rgba(var(--shadow-color), 0.2);
+  margin: 0 0 0.2em 0;
 }
 
 @media only screen and (min-width: 110vmin) {
   .social-button {
     --size: clamp(20px, 10vw, 100px);
+  }
+}
+
+@media (prefers-color-scheme: dark) {
+  .social-button {
+    background-color: rgb(25, 25, 25);
+    outline: 1pt solid rgb(30, 30, 30);
+    --shadow-color: 220, 220, 220;
   }
 }
 </style>
