af80a0d7fd12783e15d15b251dc5cd74def682dc
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2022-11-01 23:35:49 +1300

    remove unused imports

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,6 @@ import { createRouter, createWebHistory } from "vue-router";
 import generatedPages from "virtual:generated-pages";
 import { setupLayouts } from "virtual:generated-layouts";
 
-import Home from "../views/Home.vue";
-import Projects from "../views/Projects.vue";
-import MainMenu from "../views/Menu.vue";
-
 const routes = setupLayouts(generatedPages);
 
 console.log(routes);
