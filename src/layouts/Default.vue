399c607180730f96b38154528d63570267cda252
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2022-11-01 23:34:11 +1300

    created new "Empty" layout, default is now nicer, but not finished yet

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,9 +1,43 @@
 <script setup lang="ts">
+import CloseButton from '../components/CloseButton.vue';
+import LogoVue from '../components/Logo.vue';
 </script>
 
 <template>
-  <RouterView /> 
+  <div class="container">
+    <LogoVue small class="container__logo" />
+    <CloseButton class="container__close-button" />
+    <div class="container__content">
+      <RouterView />
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  padding: 1.5rem;
+  min-height: calc(100vh - 3rem);
+  height: 100%;
+  display: grid;
+  grid-template-areas:
+    "logo close-button"
+    "container container";
+  grid-template-columns: min-content auto;
+  grid-template-rows: min-content auto;
+}
+
+.container__logo {
+  grid-area: logo;
+}
+
+.container__close-button {
+  grid-area: close-button;
+  justify-self: self-end;
+}
+
+.container__content {
+  grid-area: container;
+  width: calc(100vmin - 3rem);
+  justify-self: center;
+}
 </style>
