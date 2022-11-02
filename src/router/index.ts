8cc69b5f7e392a89a13cd20b440228e66ac5b663
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2022-11-02 19:33:03 +1300

    added scroll behaviour stuff and not found route

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,13 +2,23 @@ import { createRouter, createWebHistory } from "vue-router";
 import generatedPages from "virtual:generated-pages";
 import { setupLayouts } from "virtual:generated-layouts";
 
-const routes = setupLayouts(generatedPages);
-
-console.log(routes);
+const routes = setupLayouts([
+  ...generatedPages,
+  {
+    // Not found redirects to homepage.
+    path: '/:pathMatch(.*)*',
+    redirect: '/',
+  },
+]);
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) return savedPosition;
+    if (to.hash) return { el: to.hash };
+    return { left: 0, top: 0 };
+  },
 });
 
 export default router;
