dfee646a2b5c9013b59e0315314cddbd531bd7aa
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2023-02-26 20:41:45 +1300

    hacky redirect old domain

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,9 @@ import router from "./router";
 import ImageGallery from "./components/ImageGallery.vue";
 import MarkdownWrapper from "./components/MarkdownWrapper.vue";
 
+if (window.location.hostname == 'generatively.xyz')
+  window.location.href = 'http://lucasrs.dev' + window.location.pathname + window.location.search
+
 createApp(App)
   .use(createHead({ titleTemplate: "%s - Lucas RiedlShah" }))
   .use(router)
